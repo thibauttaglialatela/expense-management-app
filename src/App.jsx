2b4f84a56ce69a7bb38cbc3dce3f2607b325b5ad
thibauttaglialatela <thibauttaglialatela@gmail.com>
2024-03-15 10:31:42 +0100

    📦 NEW: start project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="mb-2 mt-0 text-5xl font-medium leading-tight text-primary">
+        Mon application de gestion des dépenses
+      </h1>
+    </>
+  );
+}
+
+export default App;
